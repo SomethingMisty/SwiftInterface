f100f7a7d03ee218097d5dc96e491380ec0c400a
SomethingMisty <85935806+SomethingMisty@users.noreply.github.com>
2021-08-02 16:22:58 +0300

    Create ShadowView.swift

---
+++ Application/CustomElements/ShadowView.swift
@@ -0,0 +1,59 @@
+//
+//  ShadowView.swift
+//  Application
+//
+//  Created by Alex Gikaev on 01.08.2021.
+//
+
+import UIKit
+
+
+@IBDesignable
+class ShadowView: UIImageView {
+    
+    
+    @IBInspectable var shadowColor: UIColor? {
+        set {
+            layer.shadowColor = newValue!.cgColor
+        }
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor:color)
+            }
+            else {
+                return nil
+            }
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        set {
+            layer.shadowOpacity = newValue
+        }
+        get {
+            return layer.shadowOpacity
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGPoint {
+        set {
+            layer.shadowOffset = CGSize(width: newValue.x, height: newValue.y)
+        }
+        get {
+            return CGPoint(x: layer.shadowOffset.width, y:layer.shadowOffset.height)
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        set {
+            layer.shadowRadius = newValue
+        }
+        get {
+            return layer.shadowRadius
+        }
+    }
+
+}
+
+
+
